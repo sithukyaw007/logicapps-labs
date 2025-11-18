9739a88d29c03f8deb3de8674eb9158b6f2f024d
Sithu Kyaw RemoteDev <sithukyaw@microsoft.com>
2025-11-18 16:18:54 +0000

    update Dev Container configs

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install -y \
   curl \
   git \
-  gnupg2 \
+  gnupg \
   jq \
   sudo \
   zsh
@@ -23,8 +23,7 @@ nvm current # Should print "v22.14.0".
 npm -v # Should print "10.9.2".
 
 ## Setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Set default shell to zsh for root
+chsh -s $(which zsh) root || true
